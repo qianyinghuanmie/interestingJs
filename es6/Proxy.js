ced3544d2c37d938d58408b0185227e2494ab860
aab_100007443258_starwindxw <aab_100007443258_starwindxw@git.cloud.tencent.com>
2019-01-25 18:28:00 +0800

    1

--- es6/Proxy.js
+++ es6/Proxy.js
@@ -1,21 +1,59 @@
-let validator = {
-  set: function(obj, prop, value) {
-    if (prop === 'age') {
-      if (!Number.isInteger(value)) {
-        throw new TypeError('The age is not an integer');
-      }
-      if (value > 200) {
-        throw new RangeError('The age seems invalid');
-      }
-    }
+// let validator = {
+//   set: function(obj, prop, value) {
+//     if (prop === 'age') {
+//       if (!Number.isInteger(value)) {
+//         throw new TypeError('The age is not an integer');
+//       }
+//       if (value > 200) {
+//         throw new RangeError('The age seems invalid');
+//       }
+//     }
+//
+//     // The default behavior to store the value
+//     obj[prop] = value;
+//   }
+// };
+//
+// let person = new Proxy({}, validator);
+//
+// person.age = 100;
+//
+// console.log(person.age);
 
-    // The default behavior to store the value
-    obj[prop] = value;
-  }
+function extend(sup,base) {
+  var descriptor = Object.getOwnPropertyDescriptor(
+    base.prototype,"constructor"
+  );
+  console.log(descriptor);
+  base.prototype = Object.create(sup.prototype);
+  var handler = {
+    construct: function(target, args) {
+      var obj = Object.create(base.prototype);
+      this.apply(target,obj,args);
+      return obj;
+    },
+    apply: function(target, that, args) {
+      sup.apply(that,args);
+      base.apply(that,args);
+    }
+  };
+  var proxy = new Proxy(base,handler);
+  descriptor.value = proxy;
+  Object.defineProperty(base.prototype, "constructor", descriptor);
+  return proxy;
+}
+
+var Person = function(name){
+  this.name = name
 };
 
-let person = new Proxy({}, validator);
+var Boy = extend(Person, function(name, age) {
+  this.age = age;
+});
 
-person.age = 100;
+Boy.prototype.sex = "M";
 
-console.log(person.age);
+var Peter = new Boy("Peter", 13);
+console.log(Peter.sex);  // "M"
+console.log(Peter.name); // "Peter"
+console.log(Peter.age);  // 13
