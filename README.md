b31e59f4c423988c7d5a023e9f9109186a78050e
starwind <1395315573@qq.com>
2019-05-14 15:12:42 +0800

    2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# interestingJs
+  # interestingJs
 Some basic and interesting knowledge of JavaScript
 - [webgl篇](https://github.com/qianyinghuanmie/interestingJs/tree/master/webgl)
 - [canvas篇](https://github.com/qianyinghuanmie/interestingJs/tree/master/canvas)
@@ -11,3 +11,43 @@ Some basic and interesting knowledge of JavaScript
 - [webpack](webpack/README.md)
 - 播放器篇
 - 实践篇
+
+### 前端体系方面知识点的梳理
+
+参考 [一名【合格】前端工程师的自检清单](https://juejin.im/post/5cc1da82f265da036023b628)
+
+图片来源 https://juejin.im/post/5cc1da82f265da036023b628
+
+![自检图](/img/index-1.png)
+
+#### 前端必备的(html,css,js)
+  - [js](#js)
+  -
+
+##### js
+
+- es5
+
+  参考《js高级程序的设计》
+
+  1、变量
+
+  2、函数
+
+  3、面向对象
+
+  4、数组
+    一些常用的方法
+
+
+  5、字符
+
+  6、Bom
+
+  7、Dom
+
+  8、事件
+
+  9、ajax请求等等
+
+- es6篇
--- arithmetic/test.js
+++ arithmetic/test.js
@@ -1,32 +1,17 @@
-
-var quickSort = function(arr) {
-
-　　if (arr.length <= 1) { return arr; }//数组为1的时候返回原数组
-
-    var pivotIndex = Math.floor(arr.length / 2) ;
-
-    var pivot = arr.splice(pivotIndex, 1)[0]; //基准数字
-
-    var left = [];
-
-    var right = [];
-
-    for(var i =0;i<arr.length;i++){
-
-      if(arr[i]<pivot){
-
-        left.push(arr[i])
-
-      }else{
-
-        right.push(arr[i])
-      }
-    }
-
-    // return quickSort(left).concat([pivot], quickSort(right));
-    return [...quickSort(left),...[pivot],...quickSort(right)]; //递归
-
-};
-
-let arr = [33,55,11,43,52,55,56,97,54,23];
-console.log(quickSort(arr));
+function car() {
+  this.say=function () {
+    console.log("hello");
+  }
+}
+car.prototype.move=function () {
+  console.log("move111");
+}
+var car1 = new car();
+var car2 = new car();
+car1.move()
+console.log(car.prototype);
+console.log(car);
+// console.log(car1);
+car2.say=function () {
+  console.log("no");
+}
